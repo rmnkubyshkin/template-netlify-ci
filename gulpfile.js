1cd308aa7018c85b7792997690fc69feab5c60e6
rmnkubyshkin <rm.kubyshkin@gmail.com>
2024-06-15 10:55:42 +0300

    init project

---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat-css');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+const plumber = require('gulp-plumber');
+const build = gulp.series(clean, gulp.parallel(html, css, images, fonts, videos));
+const watchapp = gulp.parallel(build, watchFiles, serve);
+
+ 
+exports.clean = clean;
+exports.fonts = fonts; 
+exports.videos = videos; 
+exports.images = images; 
+exports.css = css; 
+exports.html = html;
+exports.build = build;
+exports.watchapp = watchapp;
+exports.default = watchapp;
+
+function html() {
+  return gulp.src('src/**/*.html')
+          .pipe(plumber())
+            .pipe(gulp.dest('dist/'))
+              .pipe(browserSync.reload({stream: true}));
+}
+
+
+function css() {
+  return gulp.src('src/blocks/**/*.css')
+          .pipe(plumber())
+            .pipe(concat('bundle.css'))
+              .pipe(gulp.dest('dist/'))
+                .pipe(browserSync.reload({stream: true}));
+}
+
+
+function images() {
+  return gulp.src('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}')
+            .pipe(gulp.dest('dist/images'))
+              .pipe(browserSync.reload({stream: true}));
+}
+
+
+function videos() {
+  return gulp.src('src/videos/**/*.{mp4,avi,mov,wmv,webm,mkv}')
+          .pipe(gulp.dest('dist/videos'))
+            .pipe(browserSync.reload({stream: true}));
+}
+
+
+function fonts() {
+  return gulp.src('src/fonts/**/*.{woff,woff2,ttf,otf,eot}')
+          .pipe(gulp.dest('dist/fonts'))
+            .pipe(browserSync.reload({stream: true}));
+}
+
+
+function clean() {
+  return del('dist');
+}
+
+
+function watchFiles() {
+  gulp.watch(['src/**/*.html'], html);
+  gulp.watch(['src/blocks/**/*.css'], css);
+  gulp.watch(['src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
+  gulp.watch(['src/videos/**/*.{mp4,avi,mov,wmv,webm,mkv}'], videos);
+  gulp.watch(['src/fonts/**/*.{woff,woff2,ttf,otf,eot}'], fonts);
+}
+
+function serve() {
+  browserSync.init({
+    server: {
+      baseDir: './dist'
+    }
+  });
+} 
